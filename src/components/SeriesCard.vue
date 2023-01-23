91beba8959eca15c8d0f999c756c09869f28fb05
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-23 17:00:01 +0100

    added series card component and add contents

---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'Series Card',
+    props: {
+        card: Object
+    }
+}
+</script>
+
+<template>
+    <div class="series-card">
+        <img :src="card.thumb" :alt="card.thumb">
+        <h6>{{ card.series }}</h6>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.series-card {
+    flex-basis: calc(100% / 6);
+    padding: 15px;
+
+    img {
+        display: block;
+    }
+
+    h6 {
+        text-transform: uppercase;
+        padding-top: 10px;
+    }
+}
+</style>
